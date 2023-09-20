c0c009f52471c44537a026fdff4b7410d772b141
Anna <annkrv80@yandex.ru>
2023-09-20 22:31:47 +0700

    HW_2

--- HW_2/vehicle.py
+++ HW_2/vehicle.py
@@ -1,4 +1,3 @@
-
 class Vehicle:
     def __init__(self,company:str, model:str, yearRelease:int, numWheel=0 ,speed=0):
         self.company = company
@@ -15,29 +14,26 @@ class Vehicle:
 
     def __repr__(self):
         return f"Транспортное средство {self.company}, '{self.model}', {self.yearRelease}, {self.numWheel},{self.speed}"
-    
+
+
 class Car(Vehicle):
     def __init__(self,*args, **kwargs):
         super().__init__(*args, **kwargs)
         self.numWheel = 4   
-             
-        
-
-
+                   
     def testDrive(self):
         speed_drive = 60
         self.speed = speed_drive
         
     def park(self):
         speed_parking = 0
-        self.speed = speed_parking
-    
+        self.speed = speed_parking 
+
+
 class Motorcycle(Vehicle):
     def __init__(self,*args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.numWheel = 2   
-           
-        
+        self.numWheel = 2             
 
     def testDrive(self):
         speed_drive = 75
